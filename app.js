1569ea65091a409b542f84a54f7c3d5e8b054c4c
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-07-12 14:39:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Wczytujemy dane i uruchamiamy odliczanie
+  // Load the data and start the countdown
   loadData();
   startCountdown();
 });
 
 async function loadData() {
-  // Wczytujemy dane z pliku JSON
+  // Load the data from the JSON file
   var response = await fetch('data.json');
   var data = await response.json();
 
-  // Aktualizujemy stronę na podstawie wczytanych danych
+  // Update the page with the loaded data
   document.getElementById("deposits").innerHTML = 
     data.deposits.map(deposit => `${deposit.address}: ${deposit.amount} XEC`).join('<br>');
   document.getElementById("prizePool").innerHTML = data.prizePool + " XEC";
   document.getElementById("previousWinner").innerHTML = 
-    `Adres: ${data.previousWinner.address}, wygrana: ${data.previousWinner.prize} XEC`;
+    `Address: ${data.previousWinner.address}, Prize: ${data.previousWinner.prize} XEC`;
 }
 
 function startCountdown() {
-  // Ustalamy datę i czas następnego losowania
+  // Set the date and time of the next draw
   var nextDraw = new Date();
-  nextDraw.setDate(nextDraw.getDate() + ((6 - nextDraw.getDay() + 7) % 7)); // Następna sobota
+  nextDraw.setDate(nextDraw.getDate() + ((6 - nextDraw.getDay() + 7) % 7)); // Next Saturday
   nextDraw.setHours(15);
   nextDraw.setMinutes(0);
   nextDraw.setSeconds(0);
 
-  // Uruchamiamy odliczanie
+  // Start the countdown
   var interval = setInterval(function() {
     var now = new Date().getTime();
     var distance = nextDraw - now;
 
-    // Obliczamy ile dni, godzin, minut i sekund pozostało do następnego losowania
+    // Calculate the number of days, hours, minutes, and seconds until the next draw
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Wyświetlamy odliczanie na stronie
-    document.getElementById("countdown").innerHTML = days + " dni " + hours + " godz. "
-    + minutes + " min. " + seconds + " sek.";
+    // Display the countdown on the page
+    document.getElementById("countdown").innerHTML = days + " days " + hours + " hrs "
+    + minutes + " mins " + seconds + " secs";
 
-    // Jeżeli odliczanie dobiegło końca, zatrzymujemy je
+    // If the countdown is over, stop it
     if (distance < 0) {
       clearInterval(interval);
-      document.getElementById("countdown").innerHTML = "CZAS NA LOSOWANIE!";
+      document.getElementById("countdown").innerHTML = "TIME FOR THE DRAW!";
     }
   }, 1000);
 }
