06fa6a473640cbc23425b5d6350903fdcb5c8214
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-07-12 14:31:21 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Tutaj będziemy wczytywać dane i aktualizować stronę
+});
+
+function loadData() {
+  // Tutaj będziemy wczytywać dane z pliku JSON
+}
+
+function updatePage() {
+  // Tutaj będziemy aktualizować stronę na podstawie wczytanych danych
+}
+
+function startCountdown() {
+  // Tutaj będziemy odliczać czas do następnego losowania
+}
